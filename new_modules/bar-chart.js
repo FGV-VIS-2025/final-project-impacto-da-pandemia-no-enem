2fe4278ea201ce670d66b7db2337537c0e7a2ec2
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2025-06-12 21:04:12 -0300

    createLegend modularizada

--- new_modules/bar-chart.js
+++ new_modules/bar-chart.js
@@ -1,5 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as utils from "./utils.js";
+import { createLegend } from "./legend.js";
 const {LOOKUP, width, height, margin, svg, y, barsGroup, tooltip} = utils;
 
 
@@ -149,59 +150,4 @@ export function barCharts(regions = [], data, filteredCategory) {
     else {
         title.text(" Quantidade de Inscrições do ENEM nos estados selecionados pela variável selecionada")
     }   
-}
-
-function createLegend(colorScale, column, regions, allCategories, currentFilter, data) {
-    svg.selectAll(".legend").remove();
-    
-    const legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", `translate(${width - margin.right - 180},${margin.top - 20})`);
-
-    if (currentFilter) {
-        let index = allCategories.indexOf(currentFilter);
-        if (index < 0) index = 0; 
-
-        const legendGroup = legend.append("g")
-            .attr("class", "legend-item")
-            .attr("transform", `translate(0, 0)`);
-
-        legendGroup.append("rect")
-            .attr("class", "legend-rect")
-            .attr("width", 15)
-            .attr("height", 15)
-            .attr("fill", colorScale[index % 10]);
-
-        legendGroup.append("text")
-            .attr("class", "legend-text")
-            .attr("x", 20)
-            .attr("y", 12)
-            .text(LOOKUP[column][currentFilter]);
-
-        legendGroup.on("click", function() {
-            barCharts(regions, data);
-        });
-    } else {
-        allCategories.forEach((category, i) => {
-            const legendGroup = legend.append("g")
-                .attr("class", "legend-item")
-                .attr("transform", `translate(0, ${i * 20})`);
-
-            legendGroup.append("rect")
-                .attr("class", "legend-rect")
-                .attr("width", 15)
-                .attr("height", 15)
-                .attr("fill", colorScale[i % 10]);
-
-            legendGroup.append("text")
-                .attr("class", "legend-text")
-                .attr("x", 20)
-                .attr("y", 12)
-                .text(LOOKUP[column][category]);
-
-            legendGroup.on("click", function() {
-                barCharts(regions, data, category);
-            });
-        });
-    }
 }
